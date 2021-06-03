dbdee2a873468f81268430134898e9e8987d63fc
Yakov Sinwani <yakovsinwani@gmail.com>
2021-06-03 12:20:20 +0300

    emit & props example

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,5 +1,7 @@
 <template>
   <q-page class="flex column flex-center">
+    <Brother @brother="changeTitle"/>
+    <h2>{{title}}</h2>
     <img
       class="q-ma-xl"
       alt="Quasar logo"
@@ -7,40 +9,57 @@
       style="width: 200px; height: 200px"
     >
     <q-btn label="login with google" @click="googleSing"/>
+    <TestComponent v-bind:title="title"/>
   </q-page>
 </template>
 
 <script>
 import firebaseInstance from '../middleware/firebaseAPI'
+import TestComponent from "components/TestComponent";
+import Brother from "components/Brother";
 export default {
   name: 'PageIndex',
+  components: {Brother, TestComponent},
+  created() {
+    addEventListener('brother',(e) => {
+      debugger
+    })
+  },
+  data() {
+    return {
+      title: 'My Best Title never!!!!'
+    }
+  },
   methods: {
+    changeTitle(name) {
+      this.title = name
+    },
     googleSing(){
-
-      const provider = new firebaseInstance.firebase.auth.GoogleAuthProvider();
-      firebaseInstance.firebase.auth()
-        .signInWithPopup(provider)
-        .then((result) => {
-          /** @type {firebase.auth.OAuthCredential} */
-          var credential = result.credential;
-
-          // This gives you a Google Access Token. You can use it to access the Google API.
-          var token = credential.accessToken;
-
-          // The signed-in user info.
-          var user = result.user;
-          window.user = user;
-          this.$router.push('/home')
-        }).catch((error) => {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // ...
-      });
+      this.title = 'changed';
+      // const provider = new firebaseInstance.firebase.auth.GoogleAuthProvider();
+      // firebaseInstance.firebase.auth()
+      //   .signInWithPopup(provider)
+      //   .then((result) => {
+      //     /** @type {firebase.auth.OAuthCredential} */
+      //     var credential = result.credential;
+      //
+      //     // This gives you a Google Access Token. You can use it to access the Google API.
+      //     var token = credential.accessToken;
+      //
+      //     // The signed-in user info.
+      //     var user = result.user;
+      //     window.user = user;
+      //     this.$router.push('/home')
+      //   }).catch((error) => {
+      //   // Handle Errors here.
+      //   var errorCode = error.code;
+      //   var errorMessage = error.message;
+      //   // The email of the user's account used.
+      //   var email = error.email;
+      //   // The firebase.auth.AuthCredential type that was used.
+      //   var credential = error.credential;
+      //   // ...
+      // });
 
     }
   }
